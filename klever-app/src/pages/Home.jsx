f243ace19acbf39a4413aad194c55a919a207545
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-06 15:26:31 -0300

    Build: Home structure

--- klever-app/src/pages/Home.jsx
+++ klever-app/src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import Header from "../components/Header";
+import Header from '../components/Header';
+import image from '../assets/shooting-star.svg';
 
 function Home() {
   return (
     <div>
       <Header />
+      <main>
+        <section>
+          <div>
+            <img src={ image } alt="shooting-star" />
+            <h1>Wish Wallet</h1>
+          </div>
+          <button>Add Token</button>
+        </section>
+      </main>
     </div>
   )
 }

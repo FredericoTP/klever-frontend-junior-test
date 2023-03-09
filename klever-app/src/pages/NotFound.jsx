5f9828507c2988b1ee08741c99157f2ded17c810
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-09 19:55:33 -0300

    Style: NotFound

--- klever-app/src/pages/NotFound.jsx
+++ klever-app/src/pages/NotFound.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
-import image from "../assets/error.svg"
+import image from "../assets/error.svg";
+import "../style/NotFound.css";
 
 function NotFound () {
   return (
-    <div>
-      <div>
-        <img src={ image } alt="404 NotFound" />
-        <h2>Página não encontrada!</h2>
-        <p>A página que você procura não existe ou um outro erro ocorreu.</p>
-        <Link to="/">Página Inicial</Link>
-      </div>
+    <div className="notfound-container">
+        <img className="notfound-image" src={ image } alt="404 NotFound" />
+        <h2 className="notfound-text notfound-title">Página não encontrada!</h2>
+        <p className="notfound-text notfound-p">A página que você procura não existe ou um outro erro ocorreu.</p>
+        <Link className="notfound-text notfound-btn" to="/">Página Inicial</Link>
     </div>
   )
 }
